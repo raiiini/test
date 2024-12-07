9ed42d69be3d257744f64b97e24e01d2b9b20280
raiiini <olesyamolkova@icloud.com>
2024-12-07 12:56:05 +0300

    коммит

---
+++ test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Prepodavatel
+    {
+        private int ID_Prepodavatel_int;
+        private int Familia;
+        private int Imya;
+        private int Otchestvo;
+        private int Data_rojdenia;
+        private int Adres_projivania;
+        private int Doljnsot_ID;
+
+        private void Main()
+        {
+            throw new System.NotImplementedException();
+        }
+        private void Add_info()
+        {
+            throw new System.NotImplementedException();
+        }
+        private void Edit_info()
+        {
+            throw new System.NotImplementedException();
+        }
+        private void Delete_info()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
